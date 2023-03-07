8c7e2959dcf56a3534110c596d84890489b28c2d
Nick <74491698+nickk19@users.noreply.github.com>
2023-03-07 18:35:42 +0100

    Final improvements

--- MultiServer.java
+++ MultiServer.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MultiServer {
--- ServerRunnable.java
+++ ServerRunnable.java
@@ -3,7 +3,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.ArrayList;
 
 class ServerRunnable implements Runnable {
 	Socket client = null;
